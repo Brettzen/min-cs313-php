8a9f14f3739d1517ab5f0410d219ee2309f0d4b8
Brett Starks <brettz.starks@gmail.com>
2019-02-06 11:36:10 -0700

    Team 04 assignment

--- web/view/assignments/team/w04/week4.php
+++ web/view/assignments/team/w04/week4.php
@@ -4,10 +4,10 @@
     </head>
 <body>
     <h1>Scripture Resources</h1>
-    <form method="post" action="index.php">
+    <form method="post" action="index.php?action=team4Search">
         <input type="search" name="search" id="search" placeholder="Enter scripture book name">
         <button type="submit" class="btn btn-submit">Submit</button>
-        <input type="hidden" name="action" value="team4Search">
+        <!--<input type="hidden" name="action" value="team4Search">-->
     </form>
     
 </body>    
