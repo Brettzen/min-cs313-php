b68cc4e33e612c7ff961a3a1f4fb74b1d6c1ee1e
Brett Starks <brettz.starks@gmail.com>
2019-02-06 11:04:21 -0700

    adding DB connection

--- web/lib/connections.php
+++ web/lib/connections.php
@@ -15,8 +15,6 @@ try
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-  echo '<h1>CONNECTED TO DB!!!</h1>';
 }
 catch (PDOException $ex)
 {
